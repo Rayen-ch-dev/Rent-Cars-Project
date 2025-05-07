b9e01f282717c7c72f018e13b52081ba901ad86e
rayen <challoufrayen09@gmail.com>
2025-05-07 07:50:04 +0100

    add pages and change the logo of landing page

---
+++ app/Services/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const pages = () => {
+  return (
+    <div>
+      <h1>Services</h1>
+      
+    </div>
+  )
+}
+
+export default pages
--- components/Header.tsx
+++ components/Header.tsx
@@ -11,7 +11,7 @@ const Header = async () => {
     <header className="flex justify-between items-center py-4">
       <div className="flex items-center gap-2 rounded-lg shadow-md hover:shadow-lg transition-all duration-300">
         <Image
-          src="/images/ars.png"
+          src="/images/logo.png"
           alt="logo"
           width={40}
           height={40}
@@ -27,19 +27,19 @@ const Header = async () => {
           Home
         </Link>
         <Link
-          href="/services"
+          href="/Services"
           className="hover:text-gray-300 hover:underline decoration-2"
         >
           Services
         </Link>
         <Link
-          href="/contact"
+          href="/Contact"
           className="hover:text-gray-300 hover:underline decoration-2"
         >
           Contact
         </Link>
         <Link
-          href="/about"
+          href="/About"
           className="hover:text-gray-300 hover:underline decoration-2"
         >
           About
@@ -52,7 +52,7 @@ const Header = async () => {
             <div className="flex ml-4 justify-center items-center gap-2">
               {session.user.name}
               <Image
-                src={session.user.image || "/images/user.png"}
+                src={session.user.image || "/images/userIcon.png"}
                 alt="profile"
                 width={40}
                 height={40}
@@ -65,9 +65,15 @@ const Header = async () => {
           <>
             <Link
               href="/LogIn"
-              className="border hover:bg-white hover:text-black transition duration-300 text-white mr-4 px-4 py-2 rounded-md"
+              className="border hover:bg-white hover:text-black transition duration-300 text-bl px-4 py-2 rounded-md"
             >
               Sing In
+            </Link>
+            <Link
+              href="/SignUp"
+              className="border hover:bg-black hover:text-white transition duration-300 text-black mr-4 px-4 py-2 bg-amber-50 rounded-md"
+            >
+              Sing Up
             </Link>
           </>
         )}
